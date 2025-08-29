bd6b834294e8f99470436ad6a6e2dba9a636a5dd
Enzo Lima <olucca104@gmail.com>
2025-08-29 11:31:25 -0300

    feat: Resolve o desafio '[02-verificador-de-palindromo]'

--- 01-soma-de-dois-numeros/Soma de Dois Números/src/br/com/Soma.java
+++ 01-soma-de-dois-numeros/Soma de Dois Números/src/br/com/Soma.java
@@ -1,11 +1,13 @@
+package br.com;
+
 import java.util.Arrays;
 
 public class Soma {
-    public static int[] soma(int[] nums, int target){
+    public static int[] soma(int[] nums, int target) {
         int[] valores;
-        for(int i=0; i<nums.length; i++){
-            if ((nums[i] + nums[i+1] == target) && (i+1<=nums.length-1)){
-                valores = new int[] {i,i+1};
+        for (int i = 0; i < nums.length; i++) {
+            if ((nums[i] + nums[i + 1] == target) && (i + 1 <= nums.length - 1)) {
+                valores = new int[] { i, i + 1 };
                 return valores;
             }
         }
@@ -13,7 +15,7 @@ public class Soma {
     }
 
     public static void main(String[] args) {
-        int[] numeros = {2,7,11,15};
+        int[] numeros = { 2, 7, 11, 15 };
         int target = 9;
         int[] soma = soma(numeros, target);
         System.out.println(Arrays.toString(soma));
---
+++ 02-verificador-de-palindromo/br/com/VerificadorPalindromo.java
@@ -0,0 +1,25 @@
+package br.com;
+
+public class VerificadorPalindromo {
+    public static boolean palindromoCheck(String f) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : f.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                sb.append(Character.toLowerCase(c));
+            }
+        }
+        String s = sb.toString();
+
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String f = " ";
+        System.out.println(VerificadorPalindromo.palindromoCheck(f));
+    }
+}
